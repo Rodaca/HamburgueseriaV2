9544fdbef7bfe101768d8af6b9244db1878b74d1
Rodacay <rodaca97@hotmail.com>
2023-09-14 09:57:31 -0500

    feat: se finaliza con 32 end point

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = 3000;
 const client = new MongoClient('mongodb+srv://cristian:12345@hamburgueseriacluster0.sj6qm3w.mongodb.net/');
 const db = client.db('hamburgueseria');
 
+
 const path = {
     generalPath:'/api'
 }
@@ -274,21 +275,143 @@ app.use(path.generalPath, router.get('/q22', async (req, res)=>{
     }
 }));
 
-// Encontrar todas las hamburguesas que contienen “Tomate” o “Lechuga” como ingredientes
-// Listar todos los chefs excepto “ChefA”
-// Incrementar en $2 el precio de todas las hamburguesas de la categoría “Gourmet”
-// Listar todos los ingredientes en orden alfabético
-// Encontrar la hamburguesa más cara
-// Agregar “Pepinillos” a todas las hamburguesas de la categoría “Clásica”
-// Eliminar todos los chefs que tienen una especialidad en “Cocina Vegetariana”
-// Encontrar todas las hamburguesas que contienen exactamente 7 ingredientes
-// Encontrar la hamburguesa más cara que fue preparada por un chef especializado en “Gourmet”
-// Listar todos los ingredientes junto con el número de hamburguesas que los contienen
-// Listar los chefs junto con el número de hamburguesas que han preparado
-// Encuentra la categoría con la mayor cantidad de hamburguesas
-// Listar todos los chefs y el costo total de ingredientes de todas las hamburguesas que han preparado
-// Encontrar todos los ingredientes que no están en ninguna hamburguesa
-// Listar todas las hamburguesas con su descripción de categoría
-// Encuentra el chef que ha preparado hamburguesas con el mayor número de ingredientes en total
-// Encontrar el precio promedio de las hamburguesas en cada categoría
-// Listar los chefs y la hamburguesa más cara que han preparado
+// 23 Encontrar todas las hamburguesas que contienen “Tomate” o “Lechuga” como ingredientes
+app.use(path.generalPath, router.get('/q23', async (req, res)=>{
+    try {
+        const collection = db.collection('hamburguesas');
+        const result = await collection.find({$or : [{ingredientes: 'Tomate'}, {ingredientes: 'Lechuga'}]}).toArray();        
+        res.send(result);
+
+    } catch (error) {
+        console.log(error);
+    }
+}));
+
+// 24 Listar todos los chefs excepto “ChefA”
+app.use(path.generalPath, router.get('/q24', async (req, res)=>{
+    try {
+        const collection = db.collection('chefs');
+        const result = await collection.find({nombre: {$ne: 'ChefA'}}).toArray();
+        res.send(result);
+    } catch (error) {
+        console.log(error);
+    }
+}));
+
+// 25 Incrementar en $2 el precio de todas las hamburguesas de la categoría “Gourmet”
+app.use(path.generalPath, router.get('/q25', async (req, res)=>{
+    try {
+        const collection = db.collection('hamburguesas');
+        const result = await collection.updateMany({categoria: 'Gourmet'}, {$inc: {precio: 2}});
+        res.send(result);
+
+    } catch (error) {
+        console.log(error);
+    }
+}));
+
+// 26 Listar todos los ingredientes en orden alfabético
+app.use(path.generalPath, router.get('/q26', async (req, res)=>{
+    try {
+        const collection = db.collection('ingredientes');
+        const result = await collection.find().sort({nombre: 1}).toArray();
+        res.send(result);
+    } catch (error) {
+        console.log(error);
+    }
+}));
+
+// 27 Encontrar la hamburguesa más cara
+app.use(path.generalPath, router.get('/q27', async (req, res)=>{
+    try {
+        const collection = db.collection('hamburguesas');
+        const result = await collection.find().sort({precio: -1}).limit(1).toArray();
+        res.send(result);
+
+    } catch (error) {
+        console.log(error);
+    }
+}));
+
+// 28 Agregar “Pepinillos” a todas las hamburguesas de la categoría “Clásica”
+app.use(path.generalPath, router.get('/q28', async (req, res)=>{
+    try {
+        const collection = db.collection('hamburguesas');
+        const result = await collection.updateMany({categoria: 'Clásica'}, {$push: {ingredientes: 'Pepinillos'}});
+        res.send(result);
+
+    } catch (error) {
+        console.log(error);
+    }
+}));
+
+// 29 Eliminar todos los chefs que tienen una especialidad en “Cocina Vegetariana”
+app.use(path.generalPath, router.get('/q29', async (req, res)=>{
+    try {
+        const collection = db.collection('chefs');
+        const result = await collection.deleteMany({especialidad: 'Cocina Vegetariana'});
+        res.send(result);
+    } catch (error) {
+        console.log(error);
+    }
+}));
+
+// 30 Encontrar todas las hamburguesas que contienen exactamente 7 ingredientes
+app.use(path.generalPath, router.get('/q30', async (req, res)=>{
+    try {
+        const collection = db.collection('hamburguesas');
+        const result = await collection.find({ingredientes: {$size: 7}}).toArray();
+        res.send(result);
+
+    } catch (error) {
+        console.log(error);
+    }
+}));
+
+// 31 Encontrar la hamburguesa más cara que fue preparada por un chef especializado en “Gourmet”
+app.use(path.generalPath, router.get('/q31', async (req, res)=>{
+    try {
+        const collection = db.collection('hamburguesas');
+        const result = await collection.aggregate([
+            { $match: { chef: 'ChefC', categoria: 'Gourmet' } },
+            { $sort: { precio: -1 } },
+            { $limit: 1 }
+        ]).toArray();
+        res.send(result[0]);
+
+    } catch (error) {
+        console.log(error);
+    }
+}));
+
+// 32 Listar todos los ingredientes junto con el número de hamburguesas que los contienen
+app.use(path.generalPath, router.get('/q32', async (req, res)=>{
+    try {
+        const collection = db.collection('hamburguesas');
+        const result = await collection.aggregate([
+            { $unwind: '$ingredientes' },
+            { $group: { _id: '$ingredientes', count: { $sum: 1 } } }
+        ]).toArray();
+        res.send(result);
+    } catch (error) {
+        console.log(error);
+    }
+}));
+
+// 33 Listar los chefs junto con el número de hamburguesas que han preparado
+app.use(path.generalPath, router.get('/q33', async (req, res)=>{
+    try {
+        const collection = db.collection('chefs');
+        const result = await collection.aggregate([{$group: {_id: "$chef", cantidad: {$sum: 1}}}]).toArray();
+        res.send(result);
+    } catch (error) {
+        console.log(error);
+    }
+}));
+// 34 Encuentra la categoría con la mayor cantidad de hamburguesas
+// 35 Listar todos los chefs y el costo total de ingredientes de todas las hamburguesas que han preparado
+// 36 Encontrar todos los ingredientes que no están en ninguna hamburguesa
+// 37 Listar todas las hamburguesas con su descripción de categoría
+// 38 Encuentra el chef que ha preparado hamburguesas con el mayor número de ingredientes en total
+// 39 Encontrar el precio promedio de las hamburguesas en cada categoría
+// 40Listar los chefs y la hamburguesa más cara que han preparado
